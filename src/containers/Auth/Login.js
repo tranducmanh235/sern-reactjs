a39a1be6a3b30d77423a690c116d8c334d329e54
Tran Duc Manh <tranducmanh235@gmail.com>
2022-11-17 08:57:57 +0700

    completed feature login reactjs + nodejs

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -7,6 +7,9 @@ import * as actions from "../../store/actions";
 import "./Login.scss";
 import { FormattedMessage } from "react-intl";
 
+import { handleLoginApi } from "../../services/userService";
+import { userLoginSuccess } from "../../store/actions";
+
 class Login extends Component {
     constructor(props) {
         super(props);
@@ -15,6 +18,7 @@ class Login extends Component {
             username: "",
             password: "",
             isShowPassword: false,
+            errMessage: "",
         };
     }
 
@@ -31,14 +35,37 @@ class Login extends Component {
         // console.log(e.target.value);
     };
 
-    handleLogin = () => {
-        console.log(
-            "username: ",
-            this.state.username,
-            "password: ",
-            this.state.password
-        );
-        console.log("all state", this.state);
+    handleLogin = async () => {
+        this.setState({
+            errMessage: "",
+        });
+
+        try {
+            let data = await handleLoginApi(
+                this.state.username,
+                this.state.password
+            );
+            if (data && data.errCode !== 0) {
+                this.setState({
+                    errMessage: data.message,
+                });
+            }
+            if (data && data.errCode === 0) {
+                // user login success
+                this.props.userLoginSuccess(data.user);
+                console.log("login succeed!");
+            }
+        } catch (error) {
+            if (error.response) {
+                if (error.response.data) {
+                    this.setState({
+                        errMessage: error.response.data.message,
+                    });
+                }
+            }
+
+            console.log(">>> ", error.response);
+        }
     };
 
     handleShowHidePassword = () => {
@@ -100,6 +127,9 @@ class Login extends Component {
                                 </span>
                             </div>
                         </div>
+                        <div className="col-12" style={{ color: "red" }}>
+                            {this.state.errMessage}
+                        </div>
                         <div className="col-12">
                             <button
                                 className="btn-login"
@@ -139,9 +169,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         navigate: (path) => dispatch(push(path)),
-        adminLoginSuccess: (adminInfo) =>
-            dispatch(actions.adminLoginSuccess(adminInfo)),
-        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+        // userLoginFail: () => dispatch(actions.userLoginFail()),
+        userLoginSuccess: (userInfo) =>
+            dispatch(actions.userLoginSuccess(userInfo)),
     };
 };
 
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,13 +1,13 @@
-import {combineReducers} from 'redux';
-import { connectRouter } from 'connected-react-router';
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
 
 import appReducer from "./appReducer";
 import adminReducer from "./adminReducer";
 import userReducer from "./userReducer";
 
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 
 const persistCommonConfig = {
     storage: storage,
@@ -16,13 +16,21 @@ const persistCommonConfig = {
 
 const adminPersistConfig = {
     ...persistCommonConfig,
-    key: 'admin',
-    whitelist: ['isLoggedIn', 'adminInfo']
+    key: "admin",
+    whitelist: ["isLoggedIn", "adminInfo"],
 };
 
-export default (history) => combineReducers({
-    router: connectRouter(history),
-    admin: persistReducer(adminPersistConfig, adminReducer),
-    user: userReducer,
-    app: appReducer
-})
+const userPersistConfig = {
+    ...persistCommonConfig,
+    key: "user",
+    whitelist: ["isLoggedIn", "userInfo"],
+};
+
+export default (history) =>
+    combineReducers({
+        router: connectRouter(history),
+        admin: persistReducer(adminPersistConfig, adminReducer),
+        user: persistReducer(userPersistConfig, userReducer),
+        user: userReducer,
+        app: appReducer,
+    });
